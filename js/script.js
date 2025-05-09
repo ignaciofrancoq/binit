fc6f80468d52557b9c7b594c17fca68c325bb7df
Maria Pia Zaniboni <mpiazaniboni@gmail.com>
2025-05-09 12:27:53 -0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = document.querySelector('#home__carousel');
+
+  if (slider) {
+    //new Splide('#home__carousel').mount();
+    new Splide('#home__carousel', {
+      padding: { left: '20%', right: '20%' },
+      pagination: false,
+    }).mount();
+  }  
+});
+
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.site-header');
+    if (window.scrollY > 10) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+});
