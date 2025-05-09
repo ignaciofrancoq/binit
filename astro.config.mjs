8e2450b9e9baf72f80500b8ce11b95c075673e57
Maria Pia Zaniboni <mpiazaniboni@gmail.com>
2025-05-09 14:05:52 -0300

    avances git pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "http://localhost:4321",
-  outDir: "dist"
+  site: "https://piazaniboni.github.io/binit",
+  base: process.env.NODE_ENV === 'production' ? '/binit/' : '/', //base: command === 'build' ? '/binit/' : '/',
+  outDir: 'dist',
 });
